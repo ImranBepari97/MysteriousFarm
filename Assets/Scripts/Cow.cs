2fc1b6c98690b0b54c03249499d1077af4ab29b1
amrit kandola <amritkaur304@yahoo.co.uk>
2020-07-11 22:55:12 +0100

    Added milking cows and prefab

---
+++ Assets/Scripts/Cow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cow : Interactable
+
+{
+    public bool isMilkedToday;
+    public bool isMilkable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isMilkedToday = false;
+        isMilkable = true;
+    }
+
+    public void milk() {
+        isMilkable = false;
+        isMilkedToday = true;
+    }
+
+    public void updateMilkStatus() {
+    }
+
+    public override void OnInteract(GameObject objPlayer)
+    {
+        if(isMilkable == true) {
+            milk();
+            Debug.Log("You got some milk!");
+        } else {
+            Debug.Log("Come back tommorow for more milk!");
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
