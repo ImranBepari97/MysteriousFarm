b9e5e983373a2cd79216947a583058bd28703bc6
amrit kandola <amritkaur304@yahoo.co.uk>
2020-07-11 23:23:05 +0100

    Add silo stuff

---
+++ Assets/Scripts/Silo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Silo : Interactable
+{
+    
+
+    public void sellPlant(Player player) {
+        
+        player.hasPlant = false;
+        player.plantScore += 100;
+    }
+
+    public override void OnInteract(GameObject objPlayer)
+    {
+        Player pComp = objPlayer.GetComponent<Player>();
+        if(pComp.hasPlant == true) {
+            sellPlant(pComp);
+            Debug.Log("You sold some crops!");
+        } else {
+            Debug.Log("You need crops");
+        }
+    }
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
