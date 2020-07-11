becd66b4edac2edd1fe0b2bc868050d280c0dd55
amrit kandola <amritkaur304@yahoo.co.uk>
2020-07-12 00:02:09 +0100

    bin stuff

---
+++ Assets/Scripts/bin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bin : MonoBehaviour
+{
+    public void dumpPlant() {
+        pComp.hasPlant = false
+    }
+    
+    public override void OnInteract(GameObject objPlayer)
+    {
+        Player pComp = objPlayer.GetComponent<Player>();
+        if(pComp.hasPlant == true) {
+            dumpPlant();
+        } else {
+            Debug.Log("You need crops to dump");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
