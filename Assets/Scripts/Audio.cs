b3ca4964d236c6b31544125714e93633e7bfd7b8
amrit kandola <amritkaur304@yahoo.co.uk>
2020-07-12 17:52:35 +0100

    added background music

---
+++ Assets/Scripts/Audio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    public AudioSource song;
+    // Start is called before the first frame update
+    void Start()
+    {
+         song = GetComponent<AudioSource>();
+         song.pitch = 0.65f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        song.pitch += 0.00001f;
+    }
+}
