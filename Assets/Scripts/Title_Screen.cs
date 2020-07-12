8184b0a51d42a263d3fe131871a5666f509fea43
amrit kandola <amritkaur304@yahoo.co.uk>
2020-07-12 16:09:07 +0100

    Title screen and instruction screen

---
+++ Assets/Scripts/Title_Screen.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title_Screen : MonoBehaviour
+{
+    public GameObject HelpMenu;
+    public GameObject MainMenu;
+
+
+    public void ShowMainMenu() {
+        MainMenu.SetActive(true);
+        HelpMenu.SetActive(false);
+    }
+
+    public void ShowHelpMenu() {
+        MainMenu.SetActive(false);
+        HelpMenu.SetActive(true);
+    }
+
+    public void StartGame() {
+        SceneManager.LoadScene("AmritsScene");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowMainMenu();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Cancel")) {
+            ShowMainMenu();
+        }
+    }
+}
