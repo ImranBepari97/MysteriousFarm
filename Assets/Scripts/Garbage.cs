214ad67217545967373b75130bce95a17635b06e
nickdaffern27 <45153800+nickdaffern27@users.noreply.github.com>
2020-07-12 18:21:14 +0100

    fix garbage

--- Assets/Scripts/Garbage.cs
+++ Assets/Scripts/Garbage.cs
@@ -7,6 +7,7 @@ public class Garbage : Interactable
     public void DumpPlant(Player player)
     {
         player.hasPlant = false;
+        player.whatPlantHolding = 0;
     }
 
     public override void OnInteract(GameObject objPlayer)
